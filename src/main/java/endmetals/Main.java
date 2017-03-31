c8b518a51d85f02a83f1b5cca92429a93fa1f1c3
Jasmine Iwanek <jasmine@iwanek.co.uk>
2017-03-31 13:12:36 -0700

    Update buildscripts

--- src/main/java/endmetals/Main.java
+++ src/main/java/endmetals/Main.java
@@ -35,7 +35,7 @@ public class Main {
 	public static final String MODNAME = "End Metals";
 	public static final String VERSION = "1.2.0";
 
-	/** All ore-spawn files discovered in the ore-spawn folder */
+	/* All ore-spawn files discovered in the ore-spawn folder */
 	// public static final List<Path> oreSpawnConfigFiles = new LinkedList<>();
 
 	@EventHandler
